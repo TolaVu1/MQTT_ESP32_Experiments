feb03aafd26936ba55c801cec8e6947804e8fb35
TolaVu1 <164853000+TolaVu1@users.noreply.github.com>
2024-10-31 02:16:43 +0700

    Add files via upload

---
+++ WiFi_Disconnect_QoS0_Publishing/src/mqtt.h
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace MQTT {
+    constexpr const char *username = "thanhvu";
+    constexpr const char *password = "tranthanhvu1";
+    constexpr const int port = 8883;
+    constexpr const char *broker = "aa02f20a5d494f24a63fadff434be34b.s1.eu.hivemq.cloud";
+}
---
+++ WiFi_Disconnect_QoS0_Publishing/src/wifi.h
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace WiFiSecrets {
+    const char* ssid = "POCO F5";
+    const char* pass = "12345678";
+}
+
